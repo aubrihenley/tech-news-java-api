dd50f1bb8a2d230b73f45c0da06d635e4f00d158
Aubri Henley <aubri.henley@gmail.com>
2021-12-06 16:48:07 -0500

    added folder and test files

---
+++ src/main/java/VariablesPractice.java
@@ -0,0 +1,13 @@
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int age = 37;
+        double price = 1.50;
+        double sum = age + price;
+        System.out.println("The total is " + sum);
+        String name = "Aubri";
+        String id = name + age;
+        System.out.println(id);
+
+
+    }
+}
